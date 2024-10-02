4b52a0fd665bc9fcc76748f1a665eb0333583f3c
WayahHendra <wyahhndraa@gmail.com>
2024-10-02 21:24:33 +0700

    Update Navbar

--- src/components/Header/Navbar.tsx
+++ src/components/Header/Navbar.tsx
@@ -1,24 +1,45 @@
-import Image from "next/image";
-import Link from "next/link";
+import Image from "next/image"
+import Link from "next/link"
 
 const Navbar = () => {
   return (
     <>
       <nav>
         <div className="w-100 bg-white shadow-xl">
-          <div className="py-2.5 px-16">
+          <div className="py-2.5 px-7 lg:px-16">
             <div className="flex justify-between items-center text-center text-black">
-              <div>
-                <Image
-                  src="/assets/img/logo.png"
-                  alt="logo"
-                  width={40}
-                  height={40}
-                  priority
-                />
-              </div>
               <div className="flex items-center justify-center gap-4">
-                <div>Dikirim ke : Indonesia | IDR (Rp) | ID</div>
+                <div className="lg:hidden">
+                  <Link href="#">
+                    <div className="hover:bg-gray-300 transition ease-in-out duration-300 transform w-9 h-9 flex items-center justify-center text-center rounded-xl">
+                      <Image
+                        src="/assets/icon/menu.svg"
+                        alt="search"
+                        width={20}
+                        height={20}
+                        priority
+                      />
+                    </div>
+                  </Link>
+                </div>
+                <div className="lg:hidden">
+                  <p className="text-black text-opacity-20">|</p>
+                </div>
+                <div>
+                  <Image
+                    src="/assets/img/logo.png"
+                    alt="logo"
+                    width={40}
+                    height={40}
+                    priority
+                  />
+                </div>
+              </div>
+              <div className="lg:hidden">
+                <h3 className="text-gray-900 text-base">Paradefiel</h3>
+              </div>
+              <div className="flex items-center justify-center gap-2">
+                <div className="hidden lg:block">Dikirim ke : Indonesia | IDR (Rp) | ID</div>
                 <Link href="#">
                   <div className="hover:bg-gray-300 transition ease-in-out duration-300 transform w-9 h-9 flex items-center justify-center text-center rounded-xl">
                     <Image
@@ -58,7 +79,7 @@ const Navbar = () => {
         </div>
       </nav>
     </>
-  );
-};
+  )
+}
 
-export default Navbar;
+export default Navbar
