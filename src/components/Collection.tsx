b46c09e0604f57681ef7894cc6c644326727b293
WayahHendra <wyahhndraa@gmail.com>
2024-10-08 22:10:02 +0700

    Update: Fixing bug on slidder

--- src/components/Collection.tsx
+++ src/components/Collection.tsx
@@ -1,5 +1,13 @@
+"use client"
+
 import Image from "next/image"
 import Link from "next/link"
+
+import { Swiper, SwiperSlide } from "swiper/react"
+import { Navigation, Pagination, Autoplay } from "swiper/modules"
+
+import "swiper/css"
+import "swiper/css/pagination"
 
 const Collections = () => {
     const categories = [
@@ -41,32 +49,43 @@ const Collections = () => {
     ]
 
     return (
-        <>
-            <div className="bg-[#F9F9F9]">
-                <div className="max-w-6xl mx-auto p-5">
-                    <h1 className="text-center text-2xl font-bold mb-8">Collections</h1>
-                    <div className="flex gap-4">
-                    {/* <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4"> */}
+        <div className="bg-[#F9F9F9] py-10">
+            <div className="max-w-6xl mx-auto p-5">
+                <h1 className="text-center text-2xl font-bold mb-8">Collections</h1>
+                <div className="flex justify-center items-center gap-3">
+                    <Swiper
+                        spaceBetween={10}
+                        slidesPerView={4}
+                        breakpoints={{
+                            500: { slidesPerView: 5 },
+                            768: { slidesPerView: 6 }, 
+                            1024: { slidesPerView: 7 }
+                        }}
+                        autoplay={{ delay: 4000, disableOnInteraction: false }}
+                        loop={true}
+                        modules={[Navigation, Pagination, Autoplay]}
+                        className="w-full"
+                    >
                         {categories.map((category, index) => (
-                            <Link key={index} href={category.href} className="group block relative bg-white rounded-lg shadow-lg overflow-hidden hover:shadow-xl transition-shadow duration-200">
-                                <Image
-                                    src={category.src}
-                                    alt={category.title}
-                                    // layout="responsive"
-                                    width={2000}
-                                    height={2000}
-                                    className="object-cover w-full h-48 transition-transform duration-200 transform group-hover:scale-105"
-                                />
-                                <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-                                    <h2 className="text-white text-lg text-center font-semibold">{category.title}</h2>
-                                    {/* <h2 className="text-white text-lg text-center font-semibold hidden md:block">{category.title}</h2> */}
-                                </div>
-                            </Link>
+                            <SwiperSlide key={index}>
+                                <Link href={category.href} className="group block relative bg-white rounded-lg shadow-lg overflow-hidden hover:shadow-xl transition-shadow duration-200">
+                                    <Image
+                                        src={category.src}
+                                        alt={category.title}
+                                        width={1000}
+                                        height={1000}
+                                        className="object-cover w-full h-48 transition-transform duration-200 transform group-hover:scale-105"
+                                    />
+                                    <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                                        <h2 className="text-white text-xs lg:text-base text-center font-semibold">{category.title}</h2>
+                                    </div>
+                                </Link>
+                            </SwiperSlide>
                         ))}
-                    </div>
+                    </Swiper>
                 </div>
             </div>
-        </>
+        </div>
     )
 }
 
